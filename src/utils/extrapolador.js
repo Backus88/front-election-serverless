38f6ae89e09dfc68559889398b491282ad0a81e6
Luis <lf.mc.almeida@poli.ufrj.br>
2022-10-29 02:25:37 -0300

    feat: extrapolador function

--- src/utils/extrapolador.js
+++ src/utils/extrapolador.js
@@ -1,3 +1,17 @@
-function extrapolador(
-    
-)
+function extrapolador(item){
+    let bolso = 0;
+    let lula = 0;
+    let total = 0;
+    for (const v of item){
+        if(v.uf !== 'BR'){
+            bolso += (v.bolsonaro/v.votesProportion);
+            lula += (v.lula/v.votesProportion);
+            total += (v.votesCount/v.votesProportion);
+        }
+    }
+    return({
+        bolsonaro: bolso,
+        lula: lula,
+        votosTotais: total
+    });
+}
