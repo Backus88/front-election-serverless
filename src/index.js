7ce1c57ab20e703af9fbbab699319826723cbfad
Luís Mourão <luis@phygitall.rio>
2022-10-30 08:36:49 -0300

    check css

--- src/index.js
+++ src/index.js
@@ -4,5 +4,6 @@ import App from './App'
 import './styles/reset.css'
 import './styles/style.css'
 import '../node_modules/semantic-ui-css/semantic.min.css'
+//import 'semantic-ui-css/semantic.min.css'
 
 ReactDom.render(React.createElement(App), document.querySelector('.root'))
